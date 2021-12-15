ea9e339257e58e0497ac2a0e04c9acddef550680
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 17:54:01 -0500

    fetched the pets data

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -2,6 +2,24 @@ import React from "react";
 import "../App.css";
 
 class Pets extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      pets: [],
+    }
+  }
+
+  fetchPets = () => {
+    fetch("https://pursuit-veterinarian.herokuapp.com/api/pets")
+      .then((res)=> res.json())
+      .then((data)=>{
+        this.setState({
+          pets: data,
+        })
+      })
+  }
+
   render() {
     return <div className="Pets">this is the Pets page!!</div>;
   }
