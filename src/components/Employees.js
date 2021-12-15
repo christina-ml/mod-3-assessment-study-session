5749bc93b59eea71b16c3ebb771b2f821ec5f712
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 18:43:48 -0500

    fetched employees. Added dropDownOptions. Then want to show the current employee's name

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -2,8 +2,63 @@ import React from "react";
 import "../App.css";
 
 class Employees extends React.Component {
-  render() {
-    return <div className="Employees">this is the Employees page!</div>;
+    constructor(){
+        super();
+
+        this.state = {
+            employees: [],
+            currentEmployee: null, // `null` is falsy, an `empty object` is truthy.
+        }
+    }
+
+    fetchEmployees = () => {
+        fetch("https://pursuit-veterinarian.herokuapp.com/api/employees")
+            .then((res)=> res.json())
+            .then((data)=>{
+            this.setState({
+                employees: data,
+            })
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchEmployees();
+    }
+
+    handleDropdownChange = (event) => {
+        this.setState({
+            currentEmployee: event.target.value,
+        })
+    }
+
+
+    // Map through employee data and return the name of each one
+    render() {
+    let dropDownOptions = this.state.employees.map((employee)=>{
+        return (
+            <option>{employee.firstName}</option>
+        )
+    })
+
+    let currentEmployeeObject = this.state.employees.find((employee)=>{
+        return employee.firstName === this.state.currentEmployee;
+    })
+
+    console.log(currentEmployeeObject)
+
+    return (
+    <div className="Employees">
+        <h1>this is the Employees page!</h1>
+        <select onChange={this.handleDropdownChange}>
+            <option>Select an Employee</option>
+            {dropDownOptions}
+        </select>
+        <h1>
+            {currentEmployeeObject ? currentEmployeeObject.firstName : null}
+            {currentEmployeeObject.lastName}
+        </h1>
+    </div>
+    )
   }
 }
 
