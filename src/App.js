6b3fc8506363c354d6bc07a57eaaddf3bbe0784c
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 17:19:44 -0500

    added Router to index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Pets from "./components/Pets";
+import Employees from "./components/Employees";
 
 class App extends React.Component {
   render() {
-    return <div className="App">hello world</div>;
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          
+          <Route exact path="/" component={Home}/>
+          <Route path="/pets/" component={Pets}/>
+          <Route path="/employees" component={Employees}/>
+
+        </Switch>
+      </div>
+    )
   }
 }
 
--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../App.css";
 
 class Pets extends React.Component {
   render() {
