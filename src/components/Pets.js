03beee5554b50bdf2ca47c54007ab2e590e5d39a
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 17:58:59 -0500

    mapping through our pets data with .map, and then setting to variable. Returning petsToDisplay to show it on the page

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -25,7 +25,23 @@ class Pets extends React.Component {
   }
 
   render() {
-    return <div className="Pets">this is the Pets page!!</div>;
+
+    let petsToDisplay = this.state.pets.map((pet)=>{
+      return (
+        <div className="pet-card">
+          <h2>Name: {pet.name}</h2>
+          <h3>Species: {pet.type}</h3>
+          <h3>Breed: {pet.breed}</h3>
+        </div>
+      )
+    })
+
+    return (
+      <div className="Pets">
+        <h1>this is the Pets page!!</h1>
+        <div className="pets-container">{petsToDisplay}</div>
+      </div>
+    )
   }
 }
 
